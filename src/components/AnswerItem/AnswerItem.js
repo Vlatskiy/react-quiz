3ccd7fd833eb4a1f37e589ae03cf3860af2e5f10
Vladislav Fomenko <fvladislav94@gmail.com>
2019-07-05 15:28:13 +0300

    Add quiz action (success-error answers),finished component with reset button

--- src/components/AnswerItem/AnswerItem.js
+++ src/components/AnswerItem/AnswerItem.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import classes from './AnswerItem.module.css'
 
-const AnswersItem = props => (
-    <li 
-        className={classes.AnswerItem}
-        onClick={()=> props.onAnswerClick(props.answer.id)}
-    >
-        { props.answer.text }
-    </li>
-)
+const AnswersItem = props => {
+    //Стилизация тэга li при правильном/неправильном ответе
+    const cls = [classes.AnswerItem]
+    
+    if (props.state) {
+        cls.push(classes[props.state])
+    }
+
+    return (
+        <li 
+            className={cls.join(' ')}
+            onClick={()=> props.onAnswerClick(props.answer.id)}
+        >
+            { props.answer.text }
+        </li>
+    )
+}
 
 export default AnswersItem
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,41 +1,96 @@
 import React, { Component } from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends Component {
     state = {
+        results: {}, // {[id] : success-error}
+        isFinished:false,
         activeQuestion: 0,
+        answerState: null, // {[id] : success-error}
         quiz: [
             {
                 id: 1,
                 question: 'Какого цвета небо?',
+                rightAnswerId: 2,
                 answers: [
-                    {text:'Ответ 1', id: 1},
-                    {text:'Ответ 2', id: 2},
-                    {text:'Ответ 3', id: 3},
-                    {text:'Ответ 4', id: 4}
-                ],
-                rightAnswerId: 2
+                    {text:'Желтое', id: 1},
+                    {text:'Синее', id: 2},
+                    {text:'Красное', id: 3},
+                    {text:'Черное', id: 4}
+                ]
             },
             {
                 id: 2,
                 question: 'В каком году основали Санкт-Петербург?',
+                rightAnswerId: 3,
                 answers: [
                     {text:'1700', id: 1},
                     {text:'1706', id: 2},
                     {text:'1703', id: 3},
                     {text:'1710', id: 4}
-                ],
-                rightAnswerId: 3
+                ]
             },
         ]
     }
 
-    onAnswerClickHandler = (answerId) => {
-        console.log('Answer Id:', answerId)
+    onAnswerClickHandler = answerId => {
+        //Проверка state для исключения двойного клика
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === 'success') {
+                return
+            }
+        }
 
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+        
+        //Проверка правильности ответа и переход к след.вопросу
+        if (question.rightAnswerId === answerId){
+            if(!results[question.id]) {
+                results[question.id] = 'success'
+            }
+
+            this.setState({
+                answerState:{[answerId]:'success'},
+                results
+            })
+            
+            const timeout = window.setTimeout(() => {
+                if(this.isQuizFinished()){
+                    this.setState({
+                        isFinished:true
+                    })
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+                window.clearTimeout(timeout)
+            }, 1000);
+
+        } else {
+            results[question.id] = 'error'
+            this.setState({
+                answerState:{[answerId]:'error'},
+                results
+            })
+        }
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
+    }
+
+    retryHandler = () => {
         this.setState({
-            activeQuestion: this.state.activeQuestion + 1
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
         })
     }
 
@@ -44,13 +99,25 @@ class Quiz extends Component {
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
-                    <ActiveQuiz 
-                        answers={this.state.quiz[this.state.activeQuestion].answers}
-                        question={this.state.quiz[this.state.activeQuestion].question}
-                        onAnswerClick={this.onAnswerClickHandler}
-                        quizLength={this.state.quiz.length}
-                        answerNumber={this.state.activeQuestion + 1}
-                    />
+
+                    {
+                        this.state.isFinished 
+                        ?   <FinishedQuiz
+                                results={this.state.results}
+                                quiz={this.state.quiz}
+                                onRetry={this.retryHandler}
+                            />
+                        :   <ActiveQuiz 
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                question={this.state.quiz[this.state.activeQuestion].question}
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLength={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1}
+                                state={this.state.answerState}
+                            />
+
+                    }
+
                 </div>
             </div>
         )
