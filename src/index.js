71a2c3227301e6fc79c21a1d0f14f6db46632fa7
Vladislav Fomenko <fvladislav94@gmail.com>
2019-08-05 12:49:27 +0300

    start transfering application to redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import {createStore, compose, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk'
+
+// Инициализация redux devtools
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+// Инициализация store с подключением redux devtools
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
